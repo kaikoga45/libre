8a38e3f1095d0585e9a7eab5f3bfb63e478d5c3a
Kai Koga <koga.kai@icloud.com>
2021-07-03 23:24:35 +0800

    Adding a screen that contain abou the app, the developer, and button to logout

--- lib/page/admin/admin_settings/admin_settings.dart
+++ lib/page/admin/admin_settings/admin_settings.dart
@@ -2,8 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class MemberSettings extends StatelessWidget {
-  MemberSettings({Key? key}) : super(key: key);
+class AdminSettings extends StatelessWidget {
+  AdminSettings({Key? key}) : super(key: key);
   final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
