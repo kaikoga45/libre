e9bda8a6653d0039743825bda574d89443494c2f
Kai Koga <koga.kai@icloud.com>
2021-07-03 23:11:01 +0800

    Adding new route for a list of new screen being added

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:libre/main.dart';
+import 'package:libre/page/admin/admin_database/create_update_book.dart';
+import 'package:libre/page/admin/admin_navbar/admin_nav_bar.dart';
 import 'package:libre/page/auth/onboarding.dart';
 import 'package:libre/page/auth/sign_in.dart';
 import 'package:libre/page/auth/sign_up.dart';
+import 'package:libre/page/member/display_all_book/display_all_book.dart';
+import 'package:libre/page/member/member_detail_book/member_detail_book.dart';
 import 'package:libre/page/member/member_homepage/member_homepage.dart';
 import 'package:libre/page/member/member_nav_bar/member_nav_bar.dart';
+import 'package:libre/page/member/member_shop_bag/local_widget/display_qr_code.dart';
+import 'package:libre/page/member/member_shop_bag/member_shop_bag.dart';
 
 final Map<String, Widget Function(BuildContext)> routes = {
   MemberHomepage.id: (context) => MemberHomepage(),
@@ -13,4 +19,10 @@ final Map<String, Widget Function(BuildContext)> routes = {
   SignUp.id: (context) => SignUp(),
   SignIn.id: (context) => SignIn(),
   MemberNavBar.id: (context) => MemberNavBar(),
+  AdminNavBar.id: (context) => AdminNavBar(),
+  CreateUpdateBook.id: (context) => CreateUpdateBook(),
+  MemberDetailBook.id: (context) => MemberDetailBook(),
+  MemberShopBag.id: (context) => MemberShopBag(),
+  DisplayQrCode.id: (context) => DisplayQrCode(),
+  DisplayAllBook.id: (context) => DisplayAllBook(),
 };
