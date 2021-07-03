8879db695b84df56a2c41866ff95dc0afa4e5857
Kai Koga <koga.kai@icloud.com>
2021-07-03 23:21:06 +0800

    Adding a model for temporary store book data

---
+++ lib/model/book.dart
@@ -0,0 +1,17 @@
+class Book {
+  final String? bookId;
+  final String? title;
+  final String? author;
+  final String? description;
+  final int? cost;
+  final String? urlImage;
+
+  Book({
+    this.bookId,
+    this.title,
+    this.author,
+    this.description,
+    this.cost,
+    this.urlImage,
+  });
+}
